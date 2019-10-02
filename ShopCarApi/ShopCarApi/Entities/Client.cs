823be49f227cec0832d7c96aa16d815fddf9dc20
maks.2000.2008 <maks.2000.2008@gmail.com>
2019-10-02 17:26:09 +0300

    add entities

---
+++ ShopCarApi/ShopCarApi/Entities/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Entities
+{
+    [Table("tblClients")]
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+        public float Total_price { get; set; }
+    }
+}
---
+++ ShopCarApi/ShopCarApi/Entities/Colors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Entities
+{
+    [Table("tblColors")]
+   public class Colors
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int A { get; set; }
+        [Required]
+        public int R{ get; set; }
+        [Required]
+        public int G { get; set; }
+        [Required]
+        public int B { get; set; }
+   
+
+    }
+}
