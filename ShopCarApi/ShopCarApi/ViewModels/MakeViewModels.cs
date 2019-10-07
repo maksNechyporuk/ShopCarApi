50da6e895d960182efadd8e1d68c7e7602f9baa8
maks.2000.2008 <maks.2000.2008@gmail.com>
2019-10-07 09:10:58 +0300

    add MakeVM

---
+++ ShopCarApi/ShopCarApi/ViewModels/MakeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCarApi.ViewModels
+{
+    public class MakeAddVM
+    {
+        public string Name { get; set; }
+    }
+}
