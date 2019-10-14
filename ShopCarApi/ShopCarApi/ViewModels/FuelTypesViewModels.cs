90dc80bfa0fed1e4b70bfa10eb039ae9456aff6a
maks.2000.2008 <maks.2000.2008@gmail.com>
2019-10-14 11:19:26 +0300

    add type car controller

---
+++ ShopCarApi/ShopCarApi/ViewModels/FuelTypesViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCarApi.ViewModels
+{
+    public class FuelTypeVM
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+       
+
+    }
+    public class FuelTypeAddVM
+    {
+        public string Type { get; set; }
+     
+    }
+    public class FuelTypeDeleteVM
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ShopCarApi/ShopCarApi/ViewModels/TypeViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCarApi.ViewModels
+{
+    public class TypeVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }      
+    }
+    public class TypeAddVM
+    {
+        public string Name { get; set; }
+    }
+    public class TypeDeleteVM
+    {
+        public int Id { get; set; }
+    }
+}
