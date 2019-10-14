639fba76c6c0949e7963accc8ff9a74d17ccce55
maks.2000.2008 <maks.2000.2008@gmail.com>
2019-10-14 10:15:11 +0300

    add color controller

---
+++ ShopCarApi/ShopCarApi/ViewModels/ColorViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCarApi.ViewModels
+{
+    public class ColorVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+    public class ColorAddVM
+    {
+        public string Name { get; set; }
+    }
+    public class ColorDeleteVM
+    {
+        public int Id { get; set; }
+    }
+}
