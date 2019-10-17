1f08030eb7fb4548f215bf20a1505fbdd9b4e814
tereshkovychura@gmail.com <tereshkovychura@gmail.com>
2019-10-17 12:19:38 +0300

    Delete unnecessary prop In UserLoginVM

--- ShopCarApi/ShopCarApi/ViewModels/UserViewModels.cs
+++ ShopCarApi/ShopCarApi/ViewModels/UserViewModels.cs
@@ -18,8 +18,6 @@ namespace ShopCarApi.ViewModels
     {
         public string Name { get; set; }
 
-        public string Email { get; set; }
-
         public string Password { get; set; }
     }
 
