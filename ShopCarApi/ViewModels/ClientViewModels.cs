0f578aa496a1a751f50e7b2bf6a14b10bc2e9c0c
Kirito <Kirito@A24PC2>
2019-10-29 10:12:29 +0200

    add email to client

--- ShopCarApi/ViewModels/ClientViewModels.cs
+++ ShopCarApi/ViewModels/ClientViewModels.cs
@@ -15,12 +15,15 @@ namespace ShopCarApi.ViewModels
 
         public string Image { get; set; }
 
+        public string Email { get; set; }
+
     }
     public class ClientAddVM
     {
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Image { get; set; }
+        public string Email { get; set; }
     }
     public class ClientDeleteVM
     {
